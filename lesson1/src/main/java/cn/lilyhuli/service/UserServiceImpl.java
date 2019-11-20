6d4d2ecf25d2ca9517cadf58094d2f4c36c6c380
td <lilyhuli@gmail.com>
2019-11-20 23:59:47 +0800

    把主动权交给了调用者

--- lesson1/src/main/java/cn/lilyhuli/service/UserServiceImpl.java
+++ lesson1/src/main/java/cn/lilyhuli/service/UserServiceImpl.java
@@ -4,8 +4,17 @@ import cn.lilyhuli.dao.UserDao;
 import cn.lilyhuli.dao.UserDaoImpl;
 
 public class UserServiceImpl implements UserService {
-    private UserDao userDao = new UserDaoImpl();
+    //private UserDao userDao = new UserDaoImpl();
+    private UserDao userDao;
 
+    //利用set实现
+    public void setUserDao(UserDao userDao){
+        this.userDao = userDao;
+    }
+
+
+
+    @Override
     public void getUser() {
         userDao.getUser();
     }
--- lesson1/src/test/java/Test1.java
+++ lesson1/src/test/java/Test1.java
@@ -1,3 +1,4 @@
+import cn.lilyhuli.dao.UserDaoOracleImpl;
 import cn.lilyhuli.service.UserService;
 import cn.lilyhuli.service.UserServiceImpl;
 import org.junit.Test;
@@ -9,6 +10,23 @@ public class Test1 {
 
         UserService service = new UserServiceImpl();
         service.getUser();
+
+    }
+
+    /**
+     * 那么我们要使用Oracle ,
+     * 又需要去service实现类里面修改对应的实现 .
+     * 假设我们的这种需求非常大 ,
+     * 这种方式就根本不适用了, 甚至反人类对吧 ,
+     * 每次变动 , 都需要修改大量代码 .
+     * 这种设计的耦合性太高了, 牵一发而动全身 .
+     */
+    @Test
+    public void test2() {
+        UserServiceImpl service = new UserServiceImpl();
+        service.setUserDao(new UserDaoOracleImpl());
+        service.getUser();
+
     }
 
 
